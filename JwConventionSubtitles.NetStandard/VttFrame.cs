be29292a199d0ae57937b4d440cf788e1260eed5
RomanKuschanow <kuschanow@gmail.com>
2022-07-18 12:32:30 +0300

    Crate vtt parser

---
+++ JwConventionSubtitles.NetStandard/VttFrame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VTT_Reader;
+
+public class VttFrame
+{
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public List<string> Text { get; set; }
+
+    public VttFrame(TimeSpan start_time, TimeSpan end_time, List<string> text)
+    {
+        StartTime = start_time;
+        EndTime = end_time;
+        Text = text;
+    }
+}
---
+++ JwConventionSubtitles.NetStandard/VttParser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace VTT_Reader;
+
+public class VttParser
+{
+    private List<VttFrame> frames = new();
+    public List<VttFrame> Frames { get => frames; set => frames = value; }
+
+
+    public VttParser(string path)
+    {
+        Regex regex = new Regex(@"\d{2}:\d{2}:\d{2}\.\d{3}");
+        using StreamReader sr = new StreamReader(path);
+        string? line;
+
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            MatchCollection matches = regex.Matches(line);
+
+            if (matches.Count > 0)
+            {
+
+            }
+
+            continue;
+        }
+    }
+}
